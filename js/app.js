b55678810f91760c50bb0d5af3da705eaaf45f65
Dan Rigall <danrigall@gmail.com>
2020-06-28 23:29:27 -0700

    Built a basic nav based on sections.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
+const sections = document.querySelectorAll("section");
 
+const navbarList = document.getElementById('navbar__list')
+console.log(navbarList);
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,11 +33,16 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+for (let i = 0; i <= sections.length; i++) {
+  const newListItem = document.createElement('li');
+  newListItem.innerText = sections[0 + i].dataset.nav;
 
+  navbarList.appendChild(newListItem);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,13 +53,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
