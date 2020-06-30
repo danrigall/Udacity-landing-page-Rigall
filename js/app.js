eba5e4eae8f2c42df234a1833443b8f4d51966e2
Dan Rigall <danrigall@gmail.com>
2020-06-30 11:36:56 -0700

    Created functions to highlight 'active' sections.

--- js/app.js
+++ js/app.js
@@ -18,16 +18,36 @@
  *
 */
 const sections = document.querySelectorAll("section");
+console.log(sections);
 
-const navbarList = document.getElementById('navbar__list')
+const section1 = sections[0];
+console.log(section1);
+
+const navbarList = document.getElementById('navbar__list');
 console.log(navbarList);
-
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
+document.addEventListener('scroll', addActive);
 
+function addActive() {
+  for (let i = 0; i < sections.length; i++) {
+    if (isInView(sections[i]) == true) {
+      sections[i].classList.add('your-active-class');
+    } else {
+      sections[i].classList.remove('your-active-class');
+    }
+  }
+}
+
+function isInView(elem) {
+  const boundingTop = elem.getBoundingClientRect().top;
+  if (boundingTop >= 0 && boundingTop < 400) {
+    return true;
+  }
+}
 
 
 /**
@@ -37,15 +57,23 @@ console.log(navbarList);
 */
 
 // build the nav
-for (let i = 0; i <= sections.length; i++) {
+for (let i = 0; i < sections.length; i++) {
   const newListItem = document.createElement('li');
-  newListItem.innerText = sections[0 + i].dataset.nav;
+  let sectionData = sections[i].dataset.nav;
+  let sectionID = sections[i].id;
 
+  newListItem.innerHTML = "<a href=\"#" + sectionID + "\">" + sectionData + "</a>";
   navbarList.appendChild(newListItem);
 }
 
 // Add class 'active' to section when near top of viewport
-
+// for (section of sections) {
+//   const boundingTop = section.getBoundingClientRect().top;
+//   if (boundingTop >= 0 && boundingTop < 500) {
+//     console.log(section + ' is in veiw!');
+//     section.classList.toggle('your-active-class');
+//   }
+// }
 
 // Scroll to anchor ID using scrollTO event
 
